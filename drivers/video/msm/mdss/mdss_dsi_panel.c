fdc8246ec24904f82b9734dc16bee65e923476a8
franciscofranco <franciscofranco.1990@gmail.com>
2016-01-28 01:13:45 +0000

    drivers: video: msm: mdss: Add a Simple Display State API
    Signed-off-by: franciscofranco <franciscofranco.1990@gmail.com>
    (cherry picked from commit 0c8b386ba7a4562b42567dbefb75e0538685d76b)
    Signed-off-by: Ahmad Thoriq Najahi <tlogitechnjhi@gmail.com>

--- drivers/video/msm/mdss/mdss_dsi_panel.c
+++ drivers/video/msm/mdss/mdss_dsi_panel.c
@@ -24,6 +24,8 @@
 #include <linux/string.h>
 #include <linux/hardware_info.h>
 
+#include <linux/display_state.h>
+
 #include "mdss_dsi.h"
 #include "mdss_debug.h"
 #ifdef TARGET_HW_MDSS_HDMI
@@ -41,6 +43,13 @@ extern char Lcm_name[HARDWARE_MAX_ITEM_LONGTH];
 extern bool is_Lcm_Present;
 
 DEFINE_LED_TRIGGER(bl_led_trigger);
+
+bool display_on = true;
+
+bool is_display_on()
+{
+	return display_on;
+}
 
 void mdss_dsi_panel_pwm_cfg(struct mdss_dsi_ctrl_pdata *ctrl)
 {
@@ -873,6 +882,11 @@ static int mdss_dsi_panel_on(struct mdss_panel_data *pdata)
 		return -EINVAL;
 	}
 
+	/* Ensure low persistence is disabled */
+	//mdss_dsi_panel_apply_display_setting(pdata, 0);
+
+	display_on = true;
+
 	pinfo = &pdata->panel_info;
 	ctrl = container_of(pdata, struct mdss_dsi_ctrl_pdata,
 				panel_data);
@@ -1022,6 +1036,8 @@ static int mdss_dsi_panel_off(struct mdss_panel_data *pdata)
 		mdss_dsi_panel_cmds_send(ctrl, &ctrl->off_cmds, CMD_REQ_COMMIT);
 
 	mdss_dsi_panel_off_hdmi(ctrl, pinfo);
+
+	display_on = false;
 
 end:
 	/* clear idle state */
